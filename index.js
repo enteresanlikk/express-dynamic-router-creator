3efd78b1eb942aa8021bd920ebfa25cc664ee6a1
unknown <zeki1198@gmail.com>
2018-07-27 12:56:45 +0300

    Project updated

--- index.js
+++ index.js
@@ -19,148 +19,150 @@ let OldGroupUrl=[],
 
 let newApp,newDir=process.cwd(), newMiddlewares='', newControllers='', newLog=false;
 
+
+
+ImportFile=(options)=>{
+    const {
+        mainFile,
+        folders:{
+            routers
+        }
+    } = options;
+
+    Routes=require(newDir+'\/'+routers+'\/'+mainFile);
+
+    setVariables(options,Routes);
+    List(Routes.routes);
+}
+
+setVariables=({app,folders:{routers,middlewares,controllers},log}, Routes)=>{
+    let {rootUrl,version,routes,middleware} = Routes;
+
+    newApp=app;
+
+    newRootMiddleware=middleware;
+
+    newRootUrl=rootUrl ? '/'+rootUrl : '';
+
+    if(version){
+        newVersionText=version.text ? version.text : '';
+        newVersionNumber=version.number ? version.number : '';
+        newVersion= (newVersionText || newVersionNumber) ? '/'+newVersionText+newVersionNumber : '' ;
+    }
+
+    newRoutes=routes ? routes : [];
+    ApiUrl=newRootUrl+newVersion;
+
+    newMiddlewares=middlewares;
+    newControllers=controllers;
+    newLog=log;
+}
+
+SetMiddlewares=(middleware)=>{
+    let AllRouteMiddlewares=[];
+    if(newRootMiddleware || middleware){
+        if(newRootMiddleware){
+            newRootMiddleware=newRootMiddleware.toString();
+        }else{
+            newRootMiddleware='';
+        }
+
+        if(middleware){
+            middleware=middleware.toString();
+        }else{
+            middleware='';
+        }
+    }
+
+
+
+    if(newRootMiddleware &&  middleware){
+        let test=(newRootMiddleware+','+middleware).split(',');
+        AllRouteMiddlewares=test;
+    }else if(newRootMiddleware){
+        AllRouteMiddlewares=(newRootMiddleware).split(',');
+    }else if(middleware){
+        AllRouteMiddlewares=(middleware).split(',');
+    }
+
+
+
+    AllRouteMiddlewares=AllRouteMiddlewares.filter((a,b,c)=>{
+        return c.indexOf(a)=== b;
+    });
+
+    return AllRouteMiddlewares;
+}
+
+List=(routeList)=>{
+
+   
+    const MiddlewareFolder=newMiddlewares && newMiddlewares!='' ? newDir+'/'+newMiddlewares+'/' : '';
+    const ControllerFolder=newDir+'/'+newControllers+'/';
+
+    routeList.map(route=>{
+        let keys=Object.keys(route);
+        if(keys.join(',').indexOf('group')==-1){
+            NewGroupUrl=OldGroupUrl.length>0 ? '/'+OldGroupUrl.join('/') : '';
+
+            let {
+                url,
+                controller,
+                action,
+                method,
+                middleware
+            } = route;
+            method=method.toLowerCase();
+
+
+            let FullUrl=ApiUrl+NewGroupUrl+(url ? '/'+url : '');
+            let FullControllerPath=ControllerFolder+controller;
+
+            if(fs.existsSync(FullControllerPath+'.js')){
+                if(typeof require(`${FullControllerPath}`)[`${action}`] !=='undefined'){
+
+                    let AllRouteMiddlewares=MiddlewareFolder!='' ? SetMiddlewares(middleware) : [];
+
+                    if(newLog){
+                        console.log(`[${method}] ${FullUrl} ${controller}@${action} ${(AllRouteMiddlewares && MiddlewareFolder!='' ? ((AllRouteMiddlewares.length>1) ? '| Middlewares - '+AllRouteMiddlewares.toString() : '| Middleware - '+AllRouteMiddlewares.toString()) : '')}`);
+                    }
+
+                    if(AllRouteMiddlewares.length>0 && MiddlewareFolder!=''){
+
+                        newApp[method](`${FullUrl}`
+                            ,(typeof AllRouteMiddlewares==='object')
+                                ?  AllRouteMiddlewares.map(mid=>{
+                                    return require(MiddlewareFolder+mid)
+                                })
+                                : require(MiddlewareFolder+AllRouteMiddlewares)
+                            ,require(`${FullControllerPath}`)[`${action}`]);
+                    }else{
+                        newApp[method](`${FullUrl}`,require(`${FullControllerPath}`)[`${action}`]);
+                    }
+
+
+                }else{
+                    console.log(`[${method}]`,FullUrl,action,"Action Error in",controller);
+                }
+            }else{
+                console.log(`[${method}]`,FullUrl," Controller Error");
+            }
+
+        }else{
+            OldGroupUrl.push(route.groupUrl);
+            List(route.groupRoutes,Dir);
+            OldGroupUrl=[];
+        }
+    });
+}
+
 class Index {
     constructor(){
         this.Config=this.Config.bind(this);
     }
 
     Config(options){
-        this.ImportFile(options);
-    }
-
-    ImportFile(options){
-        const {
-            mainFile,
-            folders:{
-                routers
-            }
-        } = options;
-
-        Routes=require(newDir+'\/'+routers+'\/'+mainFile);
-
-        this.setVariables(options,Routes);
-        this.List(Routes.routes);
-    }
-
-    setVariables({app,folders:{routers,middlewares,controllers},log}, Routes){
-        let {rootUrl,version,routes,middleware} = Routes;
-
-        newApp=app;
-
-        newRootMiddleware=middleware;
-
-        newRootUrl=rootUrl ? '/'+rootUrl : '';
-
-        if(version){
-            newVersionText=version.text ? version.text : '';
-            newVersionNumber=version.number ? version.number : '';
-            newVersion= (newVersionText || newVersionNumber) ? '/'+newVersionText+newVersionNumber : '' ;
-        }
-
-        newRoutes=routes ? routes : [];
-        ApiUrl=newRootUrl+newVersion;
-
-        newMiddlewares=middlewares;
-        newControllers=controllers;
-        newLog=log;
-    }
-
-    SetMiddlewares(middleware){
-        let AllRouteMiddlewares=[];
-        if(newRootMiddleware || middleware){
-            if(newRootMiddleware){
-                newRootMiddleware=newRootMiddleware.toString();
-            }else{
-                newRootMiddleware='';
-            }
-
-            if(middleware){
-                middleware=middleware.toString();
-            }else{
-                middleware='';
-            }
-        }
-
-
-
-        if(newRootMiddleware &&  middleware){
-            let test=(newRootMiddleware+','+middleware).split(',');
-            AllRouteMiddlewares=test;
-        }else if(newRootMiddleware){
-            AllRouteMiddlewares=(newRootMiddleware).split(',');
-        }else if(middleware){
-            AllRouteMiddlewares=(middleware).split(',');
-        }
-
-
-
-        AllRouteMiddlewares=AllRouteMiddlewares.filter((a,b,c)=>{
-            return c.indexOf(a)=== b;
-        });
-
-        return AllRouteMiddlewares;
-    }
-
-    List(routeList){
-
-       
-        const MiddlewareFolder=newMiddlewares && newMiddlewares!='' ? newDir+'/'+newMiddlewares+'/' : '';
-        const ControllerFolder=newDir+'/'+newControllers+'/';
-
-        routeList.map(route=>{
-            let keys=Object.keys(route);
-            if(keys.join(',').indexOf('group')==-1){
-                NewGroupUrl=OldGroupUrl.length>0 ? '/'+OldGroupUrl.join('/') : '';
-
-                let {
-                    url,
-                    controller,
-                    action,
-                    method,
-                    middleware
-                } = route;
-                method=method.toLowerCase();
-
-
-                let FullUrl=ApiUrl+NewGroupUrl+(url ? '/'+url : '');
-                let FullControllerPath=ControllerFolder+controller;
-
-                if(fs.existsSync(FullControllerPath+'.js')){
-                    if(typeof require(`${FullControllerPath}`)[`${action}`] !=='undefined'){
-
-                        let AllRouteMiddlewares=MiddlewareFolder!='' ? this.SetMiddlewares(middleware) : [];
-
-                        if(newLog){
-                            console.log(`[${method}] ${FullUrl} ${controller}@${action} ${(AllRouteMiddlewares && MiddlewareFolder!='' ? ((AllRouteMiddlewares.length>1) ? '| Middlewares - '+AllRouteMiddlewares.toString() : '| Middleware - '+AllRouteMiddlewares.toString()) : '')}`);
-                        }
-
-                        if(AllRouteMiddlewares.length>0 && MiddlewareFolder!=''){
-
-                            newApp[method](`${FullUrl}`
-                                ,(typeof AllRouteMiddlewares==='object')
-                                    ?  AllRouteMiddlewares.map(mid=>{
-                                        return require(MiddlewareFolder+mid)
-                                    })
-                                    : require(MiddlewareFolder+AllRouteMiddlewares)
-                                ,require(`${FullControllerPath}`)[`${action}`]);
-                        }else{
-                            newApp[method](`${FullUrl}`,require(`${FullControllerPath}`)[`${action}`]);
-                        }
-
-
-                    }else{
-                        console.log(`[${method}]`,FullUrl,action,"Action Error in",controller);
-                    }
-                }else{
-                    console.log(`[${method}]`,FullUrl," Controller Error");
-                }
-
-            }else{
-                OldGroupUrl.push(route.groupUrl);
-                this.List(route.groupRoutes,Dir);
-                OldGroupUrl=[];
-            }
-        });
+        ImportFile(options);
     }
 }
 
