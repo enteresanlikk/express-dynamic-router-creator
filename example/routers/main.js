d28dc79bde93118fcaa0c88714308e6f54ce9621
unknown <zeki1198@gmail.com>
2018-09-03 23:15:14 +0300

    Options Middleware system added. Group object add middleware system.

--- example/routers/main.js
+++ example/routers/main.js
@@ -1,11 +1,26 @@
 const example=require('./example');
 
+const WhiteList=YOUR_WHITE_LIST;
+var corsOptions = {
+    origin: function (origin, callback) {
+        if (WhiteList.indexOf(origin) !== -1 || !origin) {
+            callback(null, true)
+        } else {
+            callback({status:403,data:'No Access!'});
+        }
+    },
+    optionsSuccessStatus: 200,
+    methods:'OPTIONS',
+    credentials:true
+}
+
 module.exports={
     rootUrl:'api',
     version:{
         text:'v',
         number:1
     },
+    optionsMiddleware:cors(corsOptions),
     middleware:'SetHeader',
     routes:[
         {
